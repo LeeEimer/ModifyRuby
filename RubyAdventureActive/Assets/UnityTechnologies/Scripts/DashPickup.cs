9ea70389f2b54ba1b3575796f7cd4d9041802b7c
LeeEimer <AinsleyE@knights.ucf.edu>
2023-04-24 20:03:07 +0000

    Apr 24, 2023, 4:03 PM

--- RubyAdventureActive/Assets/UnityTechnologies/Scripts/DashPickup.cs
+++ RubyAdventureActive/Assets/UnityTechnologies/Scripts/DashPickup.cs
@@ -4,5 +4,21 @@ using UnityEngine;
 
 public class DashPickup : MonoBehaviour
 {
-    
+    public AudioClip collectedClip;
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        RubyController controller = other.GetComponent<RubyController>();
+
+        if (controller != null)
+        {
+
+            RubyController.dashLimit += 1;
+            Destroy(gameObject);
+
+            controller.PlaySound(collectedClip);
+
+        }
+
+    }
 }
