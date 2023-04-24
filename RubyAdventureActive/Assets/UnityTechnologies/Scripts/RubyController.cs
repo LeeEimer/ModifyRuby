d636ea8e46d1e63c656368261ae6e74063cdcd39
LeeEimer <AinsleyE@knights.ucf.edu>
2023-04-24 19:15:16 +0000

    Apr 24, 2023, 3:15 PM

--- RubyAdventureActive/Assets/UnityTechnologies/Scripts/RubyController.cs
+++ RubyAdventureActive/Assets/UnityTechnologies/Scripts/RubyController.cs
@@ -62,6 +62,7 @@ public class RubyController : MonoBehaviour
     public GameObject BotTextObj;
     public static int cogLimit; 
     public static int slowCogLimit;
+    public Boolean slowCog; 
     public Boolean winEnabled;
     public Boolean restartAllowed = false;
     public Boolean inLevel2 = false;
@@ -234,6 +235,8 @@ public class RubyController : MonoBehaviour
         GameObject projectileObject = Instantiate(projectilePrefab, rigidbody2d.position + Vector2.up * 0.5f, Quaternion.identity);
 
         Projectile projectile = projectileObject.GetComponent<Projectile>();
+
+        
         projectile.Launch(lookDirection, 300);
 
         animator.SetTrigger("Launch");
